519a31b249ed63ab571a59f9d2450439854c722a
Tomasz Mazur <47872060+AHGIJMKLKKZNPJKQR@users.noreply.github.com>
2025-10-01 17:00:12 +0200

    Include fishjam

--- deep-sea-stories/packages/web/src/main.tsx
+++ deep-sea-stories/packages/web/src/main.tsx
@@ -1,9 +1,10 @@
+import { FishjamProvider } from '@fishjam-cloud/react-client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import { TRPCClientProvider } from './contexts/trpc.tsx';
+import './index.css';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -18,7 +19,9 @@ createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<TRPCClientProvider queryClient={queryClient}>
-				<App />
+				<FishjamProvider fishjamId={import.meta.env.VITE_FISHJAM_ID}>
+					<App />
+				</FishjamProvider>
 			</TRPCClientProvider>
 		</QueryClientProvider>
 	</StrictMode>,
