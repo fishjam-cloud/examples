5d9216834d27e2167bfaa47d3c18ae8a6ca566b2
Bernard Gawor <gaworbernard@gmail.com>
2025-10-17 13:43:55 +0200

    readable-slug ref

--- deep-sea-stories/packages/web/src/lib/utils.ts
+++ deep-sea-stories/packages/web/src/lib/utils.ts
@@ -5,15 +5,20 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-const ALPHANUM =
-	'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const adjectives = [
+	'abyssal', 'deep', 'azure', 'sunken', 'coral', 'oceanic', 'tidal', 
+	'bioluminescent', 'mysterious', 'ancient', 'dark', 'glowing'
+];
 
-export function randomString(length: number = 10): string {
-	let result = '';
+const nouns = [
+	'trench', 'reef', 'anemone', 'whale', 'kraken', 'octopus', 'leviathan',
+	'shark', 'jellyfish', 'nautilus', 'squid', 'shipwreck', 'angler'
+];
 
-	for (let i = 0; i < length; i++) {
-		result += ALPHANUM.charAt(Math.floor(Math.random() * ALPHANUM.length));
-	}
-
-	return result;
+export function generateDeepSeaSlug(): string {
+	
+	const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+	const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+	
+	return `${randomAdjective}-${randomNoun}-${Math.floor(Math.random() * 100)}`;
 }
--- deep-sea-stories/packages/web/src/views/HomeView.tsx
+++ deep-sea-stories/packages/web/src/views/HomeView.tsx
@@ -1,14 +1,15 @@
 import Footer from '@/components/Footer';
 import LinkButton from '@/components/LinkButton';
 import TitleBar from '@/components/TitleBar';
-import { randomString } from '@/lib/utils';
+import { generateDeepSeaSlug } from '@/lib/utils';
+
 
 export default function HomeView() {
 	return (
 		<>
 			<TitleBar />
 			<section className="flex-1 py-16 grid place-items-center">
-				<LinkButton to={randomString()} className="font-display">
+				<LinkButton to={generateDeepSeaSlug()} className="font-display">
 					Create a game room
 				</LinkButton>
 			</section>
