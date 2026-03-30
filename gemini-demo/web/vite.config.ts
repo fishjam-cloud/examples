9772b09b163a54002fec836864455e458495579c
adrian <33912477+czerwiukk@users.noreply.github.com>
2026-03-30 11:07:27 +0200

    add better readme (#63)

--- gemini-demo/web/vite.config.ts
+++ gemini-demo/web/vite.config.ts
@@ -1,8 +1,10 @@
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  envDir: path.resolve(__dirname, ".."),
   server: {
     proxy: {
       "/api": {
